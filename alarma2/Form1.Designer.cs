c5ccb0503e9d16201ae4e53064197f17de4bdb3c
Julio7191 <99230978+julio071191@users.noreply.github.com>
2023-04-24 11:59:38 -0600

    Agregar archivos de proyecto.

---
+++ alarma2/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace alarma2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.day = new System.Windows.Forms.ComboBox();
+            this.hour = new System.Windows.Forms.ComboBox();
+            this.minute = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.add = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            this.panelAlarms = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.popUp = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.popUpText = new System.Windows.Forms.Label();
+            this.popUp.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // day
+            // 
+            this.day.FormattingEnabled = true;
+            this.day.Location = new System.Drawing.Point(12, 109);
+            this.day.Name = "day";
+            this.day.Size = new System.Drawing.Size(70, 21);
+            this.day.TabIndex = 0;
+            // 
+            // hour
+            // 
+            this.hour.FormattingEnabled = true;
+            this.hour.Location = new System.Drawing.Point(88, 109);
+            this.hour.Name = "hour";
+            this.hour.Size = new System.Drawing.Size(35, 21);
+            this.hour.TabIndex = 1;
+            // 
+            // minute
+            // 
+            this.minute.FormattingEnabled = true;
+            this.minute.Location = new System.Drawing.Point(159, 109);
+            this.minute.Name = "minute";
+            this.minute.Size = new System.Drawing.Size(35, 21);
+            this.minute.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Día";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(88, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Hora";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(156, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Minuto";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(134, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = ":";
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(12, 38);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(183, 20);
+            this.name.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Nombre";
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(15, 275);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(75, 23);
+            this.add.TabIndex = 9;
+            this.add.Text = "Agregar";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // delete
+            // 
+            this.delete.Enabled = false;
+            this.delete.Location = new System.Drawing.Point(159, 275);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(75, 23);
+            this.delete.TabIndex = 10;
+            this.delete.Text = "Eliminar";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // panelAlarms
+            // 
+            this.panelAlarms.Location = new System.Drawing.Point(423, 76);
+            this.panelAlarms.Name = "panelAlarms";
+            this.panelAlarms.Size = new System.Drawing.Size(365, 362);
+            this.panelAlarms.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(418, 16);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(219, 25);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "LISTA DE ALARMAS";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(420, 41);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(236, 17);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Seleccione para modificar o eliminar";
+            // 
+            // popUp
+            // 
+            this.popUp.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.popUp.Controls.Add(this.popUpText);
+            this.popUp.Controls.Add(this.button1);
+            this.popUp.Location = new System.Drawing.Point(256, 160);
+            this.popUp.Name = "popUp";
+            this.popUp.Size = new System.Drawing.Size(200, 100);
+            this.popUp.TabIndex = 14;
+            this.popUp.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(167, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(30, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // popUpText
+            // 
+            this.popUpText.AutoSize = true;
+            this.popUpText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.popUpText.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.popUpText.Location = new System.Drawing.Point(15, 38);
+            this.popUpText.Name = "popUpText";
+            this.popUpText.Size = new System.Drawing.Size(51, 20);
+            this.popUpText.TabIndex = 1;
+            this.popUpText.Text = "label8";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.popUp);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.panelAlarms);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.minute);
+            this.Controls.Add(this.hour);
+            this.Controls.Add(this.day);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.popUp.ResumeLayout(false);
+            this.popUp.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox day;
+        private System.Windows.Forms.ComboBox hour;
+        private System.Windows.Forms.ComboBox minute;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.Panel panelAlarms;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel popUp;
+        private System.Windows.Forms.Label popUpText;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ alarma2/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
+namespace alarma2
+{
+    using System.Timers;
+    public partial class Form1 : Form
+    {
+        private Timer minuteTimer;
+ 
+        public List<List<string>> alarms = new List<List<string>>();
+        public int counter = 0;
+        public int currentAlarm = -1;
+        public List<string> daysOfWeek = new List<string>() { "Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"};
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Alarma";
+
+            day.Items.Add("Domingo");
+            day.Items.Add("Lunes");
+            day.Items.Add("Martes");
+            day.Items.Add("Miercoles");
+            day.Items.Add("Jueves");
+            day.Items.Add("Viernes");
+            day.Items.Add("Sabado");
+            
+            day.SelectedIndex = (int)DateTime.Now.DayOfWeek;
+           
+            for (int i = 1; i < 25; i++)
+            {
+                if (i < 10)
+                {
+                    hour.Items.Add("0"+i);
+                }
+                else
+                {
+                    hour.Items.Add(i);
+                }
+                
+            }
+            
+            hour.SelectedItem = DateTime.Now.Hour;
+            for (int i = 00; i < 60; i++)
+            {
+                if (i < 10)
+                {
+                    minute.Items.Add("0"+i);
+                }
+                else { minute.Items.Add(i); }
+            }
+            minute.SelectedItem = DateTime.Now.Minute;
+
+            minuteTimer = new Timer(10000);
+            minuteTimer.Elapsed += oneMinute;
+            minuteTimer.AutoReset = true;
+            minuteTimer.Start();
+        }
+
+        private void oneMinute(object sender, ElapsedEventArgs e)
+        {
+            updatePanel();
+        }
+        
+        private void add_Click(object sender, EventArgs e)
+        {
+            if (currentAlarm == -1)
+            {
+                List<string> list = new List<string>();
+                list.Add(name.Text);
+                list.Add(day.Text);
+                list.Add(hour.Text);
+                list.Add(minute.Text);
+                list.Add("0");
+                list.Add("0");
+                alarms.Add(list);
+            }
+            else
+            {
+                alarms[currentAlarm][0] = name.Text;
+                alarms[currentAlarm][1] = day.Text;
+                alarms[currentAlarm][2]=hour.Text;
+                alarms[currentAlarm][3] = minute.Text;
+                alarms[currentAlarm][4] = "0";
+                alarms[currentAlarm][5] = "0";
+            }
+            updatePanel();
+
+        }
+        public void updatePanel()
+        {
+            
+            if (panelAlarms.InvokeRequired)
+            {
+                panelAlarms.Invoke(new MethodInvoker(updatePanel));
+            }
+            else
+            {
+                panelAlarms.Controls.Clear();
+            
+            int i = 0;
+                foreach (var alarmV in alarms)
+                {
+                    Label labelTemp = new Label();
+                    labelTemp.Text = alarmV[0] + "     " + alarmV[1] + "     " + alarmV[2] + ":" + alarmV[3];
+                    if (alarmV[4] == "1" && alarmV[5] == "1")
+                    {
+                        alarmV[4] = "0";
+                        popUpText.Text = "Alarma " + alarmV[0] + " desactivada";
+                        popUp.Visible = true;
+                    }
+                    if (alarmV[1] == daysOfWeek[(int)DateTime.Now.DayOfWeek] && int.Parse(alarmV[2]) == DateTime.Now.Hour && int.Parse(alarmV[3]) == DateTime.Now.Minute && alarmV[4] == "0" && alarmV[5] == "0")
+                    {
+                        alarmV[5] = "1";
+                        alarmV[4] = "1";
+                        popUpText.Text = "Alarma " + alarmV[0] + " activada";
+                        popUp.Visible = true;
+                        
+                    }
+                    if (alarmV[4] == "1") {
+                        labelTemp.Text += "   Activada";
+                    }
+                    else if (alarmV[4] == "0" && alarmV[5] == "1")
+                    {
+                        labelTemp.Text += "   Desactivada";
+                    }
+                labelTemp.Location = new Point(10, i*30);
+                labelTemp.Size = new Size(300, 25);
+                if (i == currentAlarm)
+                {
+                    labelTemp.BackColor = Color.Gray;
+                }
+                else
+                {
+                    labelTemp.BackColor = Color.LightGray;
+                }
+                panelAlarms.Controls.Add(labelTemp);
+                panelAlarms.AutoScroll = true;
+                labelTemp.Tag = i;
+                labelTemp.Click += new EventHandler(alarmClick);
+                i += 1;
+            }
+            }
+        }
+
+        
+        private void alarmClick(object sender, EventArgs e)
+        {
+            add.Text = "Editar";
+            delete.Enabled = true;
+            foreach (Control control in panelAlarms.Controls)
+            {
+                control.BackColor = Color.LightGray;
+            }
+            (sender as Label).BackColor = Color.Gray;
+            int count = (int)(sender as Label).Tag;
+            currentAlarm = count;
+            name.Text = alarms[count][0];
+            day.SelectedItem = alarms[count][1];
+            hour.SelectedItem = alarms[count][2];
+            minute.SelectedItem = alarms[count][3];
+
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+            
+            alarms.RemoveAt(currentAlarm);
+            currentAlarm = -1;
+            delete.Enabled = false;
+            add.Text = "Agregar";
+            updatePanel();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if(!panelAlarms.Bounds.Contains(e.Location))
+            {
+                delete.Enabled = false;
+                currentAlarm = -1;
+                delete.Enabled = false;
+                add.Text = "Agregar";
+                updatePanel();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            popUp.Visible = false;
+            updatePanel();
+        }
+    }
+}
